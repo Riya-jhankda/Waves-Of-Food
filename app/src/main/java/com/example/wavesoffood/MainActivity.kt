40d84213aaadd3b3671fcc9e51c5d636d85f55b7
Riya <riyavermajhankda200988@gmail.com>
2024-02-04 15:25:12 +0530

    navigation bar with its fragments

--- app/src/main/java/com/example/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.wavesoffood
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigation_bar= findNavController(R.id.fragmentContainerView)
+        val bottomnavview=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnavview.setupWithNavController(navigation_bar)
     }
 }
