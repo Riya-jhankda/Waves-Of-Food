7e8f40f1cc41e0d7295e1c84e2de86b7e3213c62
Riya <riyavermajhankda200988@gmail.com>
2024-02-14 06:49:51 +0530

    cart done

--- app/src/main/java/com/example/wavesoffood/adapter/cardAdapter.kt
+++ app/src/main/java/com/example/wavesoffood/adapter/cardAdapter.kt
@@ -3,14 +3,21 @@ package com.example.wavesoffood.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wavesoffood.R
 
 class cardAdapter(private val cartItems:MutableList<String>, private val CartItemPrice: MutableList<String>, private var cartImage :MutableList<Int>): RecyclerView.Adapter<cardAdapter.cardViewHolder>() {
-
+    private  val itemQuantities=IntArray(cartItems.size){1}
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): cardViewHolder {
+
+        //LayoutInflater.from(parent.context)-> calling the layoutInflater from this parent class
+        //false indicates that the inflated layout should not be immediately attached to the parent ViewGroup (RecyclerView).
+
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
-        return cardAdapter.cardViewHolder(itemView)
+        return cardViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
@@ -18,9 +25,63 @@ class cardAdapter(private val cartItems:MutableList<String>, private val CartIte
     }
 
     override fun onBindViewHolder(holder: cardViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(position)
     }
-    class cardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+    inner class cardViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView)  {
+
+        val foodNameText = itemView.findViewById<TextView>(R.id.cartFoodname)
+        val foodImageView = itemView.findViewById<ImageView>(R.id.cartimg)
+        val cpTextView = itemView.findViewById<TextView>(R.id.cartPrice)
+        val cartItemQnt= itemView.findViewById<TextView>(R.id.Cartitemqnt)
+        fun bind(position: Int) {
+            val quantity=itemQuantities[position]
+            foodNameText.text=cartItems[position]
+            foodImageView.setImageResource(cartImage[position])
+            cpTextView.text=CartItemPrice[position]
+            cartItemQnt.text=quantity.toString()
+
+            //buttons
+            val minus=itemView.findViewById<ImageButton>(R.id.minusbtn)
+            val plus = itemView.findViewById<ImageView>(R.id.plusbtn)
+            val dlt=itemView.findViewById<ImageButton>(R.id.trashbtn)
+
+            minus.setOnClickListener {
+               deceaseQuantity(position)
+            }
+
+            plus.setOnClickListener {
+                increaseQuantity(position)
+            }
+
+            dlt.setOnClickListener {
+                val itemPosistion=adapterPosition//it tell us the current position odf our cursor in the cart view
+                if(itemPosistion!=RecyclerView.NO_POSITION){
+                    deleteItem(position)
+                }
+            }
+
+
+        }
+        private fun increaseQuantity(position: Int) {
+            if (itemQuantities[position] < 10) {
+                itemQuantities[position]++
+                cartItemQnt.text = itemQuantities[position].toString()
+            }
+        }
+        private fun deceaseQuantity (position: Int) {
+            if (itemQuantities [position]>1) {
+                itemQuantities [position]--
+                cartItemQnt.text = itemQuantities [position].toString()
+            }
+        }
+        private fun deleteItem (position: Int) {
+            cartItems.removeAt(position)
+            cartImage.removeAt(position)
+            CartItemPrice.removeAt(position)
+            notifyItemRemoved (position)
+            notifyItemRangeChanged (position,cartItems.size)
+        }
+
 
     }
 }
--- app/src/main/java/com/example/wavesoffood/fragments/CartFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/CartFragment.kt
@@ -5,56 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wavesoffood.R
+import com.example.wavesoffood.adapter.PopularAdapter
+import com.example.wavesoffood.adapter.cardAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        //inflating the layout file of cart_fragment to be converted into object code
+        //and can be used to stode the data items
+        val rootView = inflater.inflate(R.layout.fragment_cart, container, false)
+
+
+        //creating the raw data to beinserted into the cardAdapter
+        val cartFoodName = listOf( "Burger", "sandwich", "momo", "item", "sandwich", "momo")
+        val cartItemPtice = listOf("$5", "$6", "$8", "$9","$10","$10")
+        val cartImage = listOf(R.drawable.food1,R.drawable.food2,R.drawable.food3,R.drawable.food4,R.drawable.food5,R.drawable.food2)
+
+        //creating an adapter that acts like a bridge between the rawdata and the recycler view of cart_fragmnet layout
+        val adapter= cardAdapter(ArrayList(cartFoodName),ArrayList(cartItemPtice),ArrayList(cartImage))
+
+        //refernce for the recycler view of cart
+        val cartRecyclerView = rootView.findViewById<RecyclerView>(R.id.cardRecyclerView)
+        val layoutManager = LinearLayoutManager(requireContext())
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        cartRecyclerView.layoutManager = layoutManager
+        cartRecyclerView.adapter = adapter
+        return rootView
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
