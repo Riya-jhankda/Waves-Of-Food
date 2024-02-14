309fe652a697b8868088c92356bfa133b37f299b
Riya <riyavermajhankda200988@gmail.com>
2024-02-15 03:21:24 +0530

    View menu done

--- app/src/main/java/com/example/wavesoffood/fragments/MenuBottomSheetFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/MenuBottomSheetFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wavesoffood.R
+import com.example.wavesoffood.adapter.MenuAdapter
+import com.example.wavesoffood.adapter.cardAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MenuBottomSheetFragment:BottomSheetDialogFragment() {
@@ -13,6 +18,7 @@ class MenuBottomSheetFragment:BottomSheetDialogFragment() {
         super.onCreate(savedInstances)
 
     }
+    //we just need to create a dummy data and recycler view mai data ko set krna hai
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,27 @@ class MenuBottomSheetFragment:BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_menu_bottom_sheet, container, false)
+
+        val btnBack= rootView.findViewById<ImageButton>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            dismiss()
+        }
+
+        //creating the raw data to being inserted into the manuAdapter
+        val menuFoodName = listOf( "Burger", "sandwich", "momo", "item", "sandwich", "momo")
+        val menuItemPtice = listOf("$5", "$6", "$8", "$9","$10","$10")
+        val menuImage = listOf(R.drawable.food1,R.drawable.food2,R.drawable.food3,R.drawable.food4,R.drawable.food5,R.drawable.food2)
+
+        //creating an adapter that acts like a bridge between the rawdata and the recycler view of menuBottomSheetFragmnet layout
+        val adapter= MenuAdapter(ArrayList(menuFoodName),ArrayList(menuItemPtice),ArrayList(menuImage))
+
+        //refernce for the recycler view of menu
+        val menuRecyclerView = rootView.findViewById<RecyclerView>(R.id.menuRecyclerView)
+        val layoutManager = LinearLayoutManager(requireContext())
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        menuRecyclerView.layoutManager = layoutManager
+        menuRecyclerView.adapter = adapter
 
         return rootView
     }
