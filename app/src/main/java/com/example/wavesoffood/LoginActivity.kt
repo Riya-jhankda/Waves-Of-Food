0d7bf133e60be78f066744111a8f6d1c2ce5bb3a
Riya <riyavermajhankda200988@gmail.com>
2024-03-15 06:05:59 +0530

    final commit

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -4,13 +4,40 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnLogin: Button
+
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        etEmail=findViewById(R.id.maile)
+        etPass=findViewById(R.id.passe)
+        btnLogin=findViewById(R.id.logine)
+
+        auth = Firebase.auth
+
+        btnLogin.setOnClickListener {
+            loginUser()
+        }
+
+
 
         val txt_noacc=findViewById<TextView>(R.id.donthave_acc)
 
@@ -20,5 +47,28 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    private fun loginUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+
+        // check pass
+        if (email.isBlank() || pass.isBlank() ) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/wavesoffood/SignActivity.kt
+++ app/src/main/java/com/example/wavesoffood/SignActivity.kt
@@ -4,19 +4,69 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SignActivity : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var name: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
 
+        etEmail=findViewById(R.id.signUpMail)
+        name=findViewById(R.id.signUpName)
+        etPass=findViewById(R.id.signUpPass)
+        btnSignUp=findViewById(R.id.signUpbtn)
+
+        auth = Firebase.auth
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
         val txt_acc_exist=findViewById<TextView>(R.id.alreadyacc)
+
 
         txt_acc_exist.setOnClickListener{
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        val Upname = name.text.toString()
+
+        // check pass
+        if (email.isBlank() || pass.isBlank() || Upname.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
