2947e55557d24db0ae0d10354fe941e566c5cf21
Riya <riyavermajhankda200988@gmail.com>
2024-03-15 10:49:57 +0530

    bottom navigation cpmplt

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wavesoffood
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,8 +9,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
@@ -20,6 +30,7 @@ class LoginActivity : AppCompatActivity() {
 
     // create Firebase authentication object
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +43,19 @@ class LoginActivity : AppCompatActivity() {
         btnLogin=findViewById(R.id.logine)
 
         auth = Firebase.auth
+
+        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken( "112204475825-d7jvnbjacbndqrqf67lgqfif806tuv2c.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient=GoogleSignIn.getClient(this,gso)
+
+        val gso_btn= findViewById<Button>(R.id.g_btn)
+
+        gso_btn.setOnClickListener {
+            SignInGoogle()
+        }
 
         btnLogin.setOnClickListener {
             loginUser()
@@ -47,6 +71,46 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    private fun SignInGoogle() {
+        val signInIntent=googleSignInClient.signInIntent
+
+        launcher.launch(signInIntent)
+    }
+    private val launcher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result->
+            if(result.resultCode==Activity.RESULT_OK){
+                val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if(task.isSuccessful){
+            val account:GoogleSignInAccount?=task.result
+            if(account!=null){
+                updatedUI(account)
+            }
+        }
+        else{
+            Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun updatedUI(account: GoogleSignInAccount) {
+        val credential=GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if(it.isSuccessful){
+                val intent:Intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+            }
+            else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun loginUser() {
